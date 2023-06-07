493278fd1975ca1b11b4e47f93df3491e5f5900c
Alejandro Plata <platamendietaalejandrojose@gmail.com>
2023-06-07 14:31:46 -0400

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Soy Alejandro Plata Mendieta, un apasionado desarrollador Full Stack con amplia experiencia en tecnologías web y seguridad informática. Mi conjunto de habilidades abarca lenguajes y herramientas como HTML, React, JavaScript, Tailwind, Yarn y Vite JS para el desarrollo frontend, y Node.js, Express, SQL y PostgreSQL para el backend.\n\nAunque no cuento con experiencia laboral formal, he dedicado tiempo y esfuerzo a proyectos personales tanto en desarrollo Full Stack como en pentesting. A través de estos proyectos, he logrado desarrollar aplicaciones web completas y he llevado a cabo pruebas de seguridad en diferentes entornos. Mi experiencia en pentesting incluye el uso de herramientas como Nmap, Metasploit, Burp Suite, Nikto, W3AF y SQLMap.\n\nMis proyectos personales en desarrollo Full Stack demuestran mi capacidad para crear soluciones efectivas y mi conocimiento práctico de las mejores prácticas de desarrollo. He trabajado en diferentes etapas de desarrollo, desde la planificación y el diseño hasta la implementación y el despliegue. Además, mi experiencia en pentesting me ha proporcionado una visión profunda sobre cómo proteger aplicaciones y sistemas contra posibles amenazas y vulnerabilidades.\n\nSoy un profesional proactivo y apasionado que busca constantemente aprender y mejorar mis habilidades. Estoy abierto a oportunidades que me permitan aplicar mis conocimientos en un entorno laboral desafiante. Estoy dispuesto a contribuir con mi experiencia en el desarrollo Full Stack y mis habilidades en seguridad informática para ayudar a proteger y mejorar la calidad de las aplicaciones y sistemas.\n\nSi buscas un desarrollador Full Stack comprometido con la excelencia técnica y la seguridad, estaré encantado de colaborar contigo. ¡No dudes en contactarme para discutir posibles proyectos o colaboraciones!',
+	},
+	{
+		id: 2,
+		bio: '',
+	},
+];
